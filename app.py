376124eb58ada1aa73202c8686961ad2cf5269e2
yangxu <835117276@qq.com>
2020-10-21 20:06:18 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-from flask import Flask, url_for, render_template
-from flask_sqlalchemy import SQLAlchemy # 导入扩展累
-from faker import Faker
+# -*- coding: utf-8 -*-
 import os
+
 import click
+from flask import Flask, url_for, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy # 导入扩展累
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev' # flash函数传递信息的密钥。# 等同于 app.secret_key = 'dev'
 # 在扩展类实例化前加载配置
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 db = SQLAlchemy(app) # 初始化扩展类，传入程序实例app
-
-class User(db.Model): # 保存用户信息，表名将会是 user（自动生成，小写处理）
-	id = db.Column(db.Integer, primary_key=True) # 主键
-	name = db.Column(db.String(20)) # 名字
-	
-class Movie(db.Model): # 保存电影条目信息，表名将会是 movie
-	id = db.Column(db.Integer, primary_key=True) # 主键
-	title = db.Column(db.String(60)) # 电影标题
-	year = db.Column(db.String(4)) # 电影年份
 
 @app.cli.command() # 注册为命令
 @click.option('--drop', is_flag=True, help='Create after drop.')
@@ -60,44 +53,74 @@ def forge():
 	click.echo('Done. ')
 
 
-
-@app.route('/index')
-@app.route('/home')
-def hello():
-	#return 'Welcome to My Watchlist!'
-	return '<h1>Hello Totoro</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_page(name):
-	return 'User: %s' % name
-
-
-@app.route('/test')
-def test_url_for():
-	# 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-	print(url_for('hello')) # 输出：/
-	# 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-	print(url_for('user_page', name='greyli')) # 输出：/user/greyli
-	print(url_for('user_page', name='peter')) # 输出：/user/peter
-	print(url_for('test_url_for')) # 输出：/test
-	# 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL后面。
-	print(url_for('test_url_for', num=2)) # 输出：/test?num=2
-	return 'Test page'
+class User(db.Model): # 保存用户信息，表名将会是 user（自动生成，小写处理）
+	id = db.Column(db.Integer, primary_key=True) # 主键
+	name = db.Column(db.String(20)) # 名字
+	
+class Movie(db.Model): # 保存电影条目信息，表名将会是 movie
+	id = db.Column(db.Integer, primary_key=True) # 主键
+	title = db.Column(db.String(60)) # 电影标题
+	year = db.Column(db.String(4)) # 电影年份
 
 
 @app.context_processor # 使用该装饰器注册一个模板上下文处理函数
 def inject_user(): # # 函数名可以随意修改
-	user = User.query.first()
+	user = User.query.first() # 读取用户记录
 	return dict(user=user) # 需要返回字典，等同于return {'user': user}
 
 
 @app.errorhandler(404) # 传入要处理的错误代码
 def page_not_found(e): # 接受异常对象作为参数
-	user = User.query.first()
 	return render_template('404.html'), 404 # 返回模板和状态码
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	user = User.query.first() # 读取用户记录
+	if request.method == 'POST': # 判断是否是 POST 请求
+		# 获取表单数据
+		title = request.form.get('title') # 传入表单对应输入字段的name值
+		year = request.form.get('year')
+
+		# 验证数据
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invalid input.') # 显示错误提示
+			return redirect(url_for('index')) # 重定向回主页
+
+		# 保存表单数据到数据库
+		movie = Movie(title=title, year=year) # 创建记录
+		db.session.add(movie) # 添加到数据库会话
+		db.session.commit() # 提交数据库会话
+		flash('Item created.') # 显示成功创建的提示
+		return redirect(url_for('index')) # 重定向回主页
+
+	user = User.query.first()
 	movies = Movie.query.all() # 读取所有电影记录
-	return render_template('index.html', movies=movies)
+	return render_template('index.html', user=user, movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+
+	if request.method == 'POST': # 处理编辑表单的提交请求
+		title = request.form['title'] # 要更新成的数据
+		year = request.form['year']
+
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invalid input.')
+			return redirect(url_for('edit', movie_id=movie_id)) # 重定向回对应的编辑页面
+
+		movie.title = title # 更新标题
+		movie.year = year # 更新年份
+		db.session.commit() # 提交数据库会话
+		flash('Item updated.')
+		return redirect(url_for('index')) # 重定向回主页
+
+	return render_template('edit.html', movie=movie) # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST']) # 限定只接受POST请求
+def delete(movie_id):
+	movie = Movie.query.get_or_404(movie_id) # 获取电影记录
+	db.session.delete(movie) # 删除对应的记录
+	db.session.commit() # 提交数据库会话
+	flash('Item deleted')
+	return redirect(url_for('index'))
